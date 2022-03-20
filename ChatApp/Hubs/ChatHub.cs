7cbb7add83b41512b64e14a5f5626088b365af1c
Alice <alicekaygriffin@gmail.com>
2022-03-20 15:36:22 -0400

    Add project files.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,43 @@
+﻿using ChatApp.Data;
+using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string BotUser;
+        private readonly IDictionary<string, UserConnection> Connections;
+
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            BotUser = "ToastBot";
+            Connections = connections;
+        }
+
+        public string GenerateRandomGreeting(string user)
+        {
+            Random random = new Random();
+            string[] greetings = new [] { $"{user} is here!", $"{user} has joined! Say hi.", $"Welcome, {user}! Party time!" };
+            return greetings[random.Next(greetings.Length)];
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", BotUser, GenerateRandomGreeting(userConnection.User));
+        }
+
+        public async Task SendMessage(UserMessage message) 
+        {
+            if (Connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+    }
+}
---
+++ ChatApp/Models/UserMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class UserMessage
+    {
+        public string User { get; set; }
+
+        public string Message { get; set; }
+
+        public string Room { get; set; }
+
+        public DateTime DateSent { get; set; }
+    }
+}
