bcab44d668d8fe54d6cb5fe9ed3b2e8562bf1a3c
Alice Griffin <agriffin1@uwm.com>
2022-08-08 12:08:42 -0400

    committing code in progress

---
+++ ChatApp/wwwroot/js/javascript.js
@@ -0,0 +1,15 @@
+﻿function getBoundingClientRect(id) {
+    const position = document.getElementById(id).getBoundingClientRect().toJSON();
+    return position;
+}
+
+function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+
+function setStyle(id, pos1, pos2) {
+    var todoEl = document.getElementById(id); 
+    todoEl.style.top = (todoEl.offsetTop - pos2) + "px";
+    todoEl.style.left = (todoEl.offsetLeft - pos1) + "px";
+}
